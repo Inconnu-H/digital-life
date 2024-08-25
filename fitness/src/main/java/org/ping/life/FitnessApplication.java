879ef6c7bedf7430c810774ea457e4abbf839727
Ping <huang-jiping@foxmail.com>
2024-08-25 09:11:54 +0800

    【健身模块】更新POM文件和启动类名称

--- fitness/src/main/java/org/ping/life/FitnessApplication.java
+++ fitness/src/main/java/org/ping/life/FitnessApplication.java
@@ -4,12 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Ping
+ */
 @MapperScan("org.ping.life.*.mapper")
 @SpringBootApplication
-public class DigitalLifeApplication {
+public class FitnessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DigitalLifeApplication.class, args);
+        SpringApplication.run(FitnessApplication.class, args);
     }
 
 }
